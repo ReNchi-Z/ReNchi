4076f1df6dc13cfe6d6cc56a84897920d021a287
ReNchi-Z <akasunasasori69@gmail.com>
2025-06-03 10:35:19 +0700

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -61,7 +61,7 @@ async fn main(req: Request, env: Env, _: Context) -> Result<Response> {
         .on_async("/link", link)
         .on_async("/converter", converter)
         .on_async("/:proxyip", tunnel)
-        .on_async("/Inconigto-Mode/:proxyip", tunnel)
+        .on_async("/NeOxide/:proxyip", tunnel)
         .run(req, env)
         .await
 }
